fc9e4051a1d4e3cbdafade9b699c791b66a41968
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-10-19 10:02:57 +0200

    WIP

---
+++ README.md
@@ -0,0 +1,4 @@
+Reproduce with
+
+    composer install
+    XDEBUG_MODE=coverage php ./vendor/bin/phpunit --coverage-clover cov.xml
--- tests/PipeTest.php
+++ tests/PipeTest.php
@@ -7,7 +7,6 @@ require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../src/functions.php';
 
 /**
- * XDEBUG_MODE=coverage ./vendor/bin/phpunit --coverage-text
  * XDEBUG_MODE=coverage php ./vendor/bin/phpunit --coverage-clover cov.xml
  */
 class PipeTest extends TestCase
